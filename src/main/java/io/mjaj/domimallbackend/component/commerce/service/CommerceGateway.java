41473ddfc11b14e271a8be05f832256148c475a3
Andres Grisales <andres.grisalesgg@gmail.com>
2020-08-31 21:32:41 -0500

    Create gateway for persistence of Commerce

---
+++ src/main/java/io/mjaj/domimallbackend/component/commerce/service/CommerceGateway.java
@@ -0,0 +1,11 @@
+package io.mjaj.domimallbackend.component.commerce.service;
+
+import io.mjaj.domimallbackend.component.commerce.model.Commerce;
+
+import javax.validation.constraints.NotNull;
+
+public interface CommerceGateway {
+
+    Commerce save(@NotNull Commerce commerceToCreate);
+
+}
