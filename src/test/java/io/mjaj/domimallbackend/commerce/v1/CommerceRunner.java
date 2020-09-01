e40c695152b307e4b376ae92445400b162e5e149
Andres Grisales <andres.grisalesgg@gmail.com>
2020-08-31 21:37:04 -0500

    Refactor

---
+++ src/test/java/io/mjaj/domimallbackend/commerce/v1/CommerceRunner.java
@@ -0,0 +1,14 @@
+package io.mjaj.domimallbackend.commerce.v1;
+
+
+import com.intuit.karate.junit5.Karate;
+
+
+public class CommerceRunner {
+
+
+    @Karate.Test
+    Karate testCreate(){
+        return Karate.run("create").relativeTo(getClass());
+    }
+}
